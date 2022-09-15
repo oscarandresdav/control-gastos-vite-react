6ed5d46a663ddc412ae26e2c0756de6f8b595dd8
Oscar Davila <oscar7822002@gmail.com>
2022-09-15 17:17:50 -0500

    Mostrando el Componente de Gastos si el presupuesto es válido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import NuevoPresupuesto from './NuevoPresupuesto'
+import React from "react";
+import NuevoPresupuesto from "./NuevoPresupuesto";
 
-const Header = ({presupuesto, setPresupuesto}) => {
+const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
   return (
     <header>
       <h1>Planificador de Gastos</h1>
-      <NuevoPresupuesto
-        presupuesto={presupuesto}
-        setPresupuesto={setPresupuesto}
-      />
-    </header>
-  )
-}
 
-export default Header
+      {isValidPresupuesto ? (
+        <p>Contro Presupuesto</p>
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  );
+};
+
+export default Header;
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,41 +1,45 @@
-import React, { useState } from 'react'
-import Mensaje from './Mensaje';
+import React, { useState } from "react";
+import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
-
-  const [mensaje, setMensaje] = useState('');
+const NuevoPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  const [mensaje, setMensaje] = useState("");
 
   const handlePresupuesto = (e) => {
     e.preventDefault();
 
-    if (!Number(presupuesto) || Number(presupuesto) < 0) {
-      setMensaje('No es un presupuesto válido');
-    } else {
-      setMensaje('Si es un presupuesto válido');
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje("No es un presupuesto válido");
+      return;
     }
-  }
+
+    setMensaje("");
+    setIsValidPresupuesto(true);
+  };
 
   return (
-    <div className='contenedor-presupuesto contenedor sombra'>
-      <form onSubmit={handlePresupuesto} className='formulario'>
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form onSubmit={handlePresupuesto} className="formulario">
         <div className="campo">
           <label>Definir Presupuesto</label>
-          <input 
-            className='nuevo-presupuesto'
-            type="number" 
-            placeholder='Añade tu Presupuesto'
+          <input
+            className="nuevo-presupuesto"
+            type="number"
+            placeholder="Añade tu Presupuesto"
             value={presupuesto}
-            onChange={ e => setPresupuesto(e.target.value) }
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
           />
         </div>
 
         <input type="submit" value="Añadir" />
 
-        {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
-
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NuevoPresupuesto
+export default NuevoPresupuesto;
