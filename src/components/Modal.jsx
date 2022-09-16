bb10d7c89f4a481ac75e7e8b1d4d236670f41c88
Oscar Davila <oscar7822002@gmail.com>
2022-09-16 11:20:39 -0500

    Añadiendo el Formulario de Gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,31 +1,62 @@
-import CerrarBtn from './../img/cerrar.svg';
+import CerrarBtn from "./../img/cerrar.svg";
 
-const Modal = ({setModal, animarModal, setAnimarModal}) => {
-
+const Modal = ({ setModal, animarModal, setAnimarModal }) => {
   const ocultarModal = () => {
     setAnimarModal(false);
-    
+
     setTimeout(() => {
       setModal(false);
     }, 500);
-  }
+  };
 
   return (
-    <div className='modal'>
+    <div className="modal">
       <div className="cerrar-modal">
-        <img 
-          src={CerrarBtn} 
-          alt="Cerrar modal"
-          onClick={ocultarModal}
-        />
+        <img src={CerrarBtn} alt="Cerrar modal" onClick={ocultarModal} />
       </div>
 
-      <form className={`formulario ${animarModal ? 'animar': 'cerrar'}`}>
+      <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
         <legend>Nuevo Gasto</legend>
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el nombre del gasto"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade la cantidad del gasto: ej. 300"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+
+          <select id="categoria">
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorros</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Añadir Gasto" />
       </form>
-
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
