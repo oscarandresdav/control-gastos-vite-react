6e9c9e4dbfb87ef743e7a7b290d351bad7a3edee
Oscar Davila <oscar7822002@gmail.com>
2022-09-16 09:56:50 -0500

    Creando una Animación en el Modal

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,15 @@ function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
   const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
 
   const handleNuevoGasto = () => {
-    setModal(true)
-  }
+    setModal(true);
+
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+  };
 
   return (
     <div>
@@ -31,9 +36,13 @@ function App() {
         </div>
       )}
 
-      {modal && <Modal
-                  setModal={setModal}
-                />}
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+        />
+      )}
     </div>
   );
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,13 @@
 import CerrarBtn from './../img/cerrar.svg';
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal, animarModal, setAnimarModal}) => {
 
   const ocultarModal = () => {
-    setModal(false);
+    setAnimarModal(false);
+    
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
   }
 
   return (
@@ -15,6 +19,11 @@ const Modal = ({setModal}) => {
           onClick={ocultarModal}
         />
       </div>
+
+      <form className={`formulario ${animarModal ? 'animar': 'cerrar'}`}>
+        <legend>Nuevo Gasto</legend>
+      </form>
+
     </div>
   )
 }
