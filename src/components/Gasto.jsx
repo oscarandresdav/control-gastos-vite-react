5d32c2b7659cc147ad3d7a74b1cd9199b22b4137
Oscar Davila <oscar7822002@gmail.com>
2022-09-17 11:33:07 -0500

    Mostrando el Icono de Gastos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,12 +1,30 @@
 import { formatearCantidad, formatearFecha } from "../helpers";
 
+import IconoAhorro from "./../img/icono_ahorro.svg";
+import IconoCasa from "./../img/icono_casa.svg";
+import IconoComida from "./../img/icono_comida.svg";
+import IconoGastos from "./../img/icono_gastos.svg";
+import IconoOcio from "./../img/icono_ocio.svg";
+import IconoSalud from "./../img/icono_salud.svg";
+import IconoSuscripciones from "./../img/icono_suscripciones.svg";
+
 const Gasto = ({ gasto }) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
+
+  const diccionarioIconos = {
+    ahorro: IconoAhorro,
+    comida: IconoComida,
+    casa: IconoCasa,
+    varios: IconoGastos,
+    ocio: IconoOcio,
+    salud: IconoSalud,
+    suscripciones: IconoSuscripciones,
+  };
 
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
-        {/* IMAGEN */}
+        <img src={diccionarioIconos[categoria]} alt={`Icono ${nombre}`} />
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{nombre}</p>
