c276b11991bec737fb022bacab626ea25ee99621
Oscar Davila <oscar7822002@gmail.com>
2022-09-17 16:29:00 -0500

    Añadiendo un Efecto de Swipe

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,4 +1,12 @@
 import { formatearCantidad, formatearFecha } from "../helpers";
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
 
 import IconoAhorro from "./../img/icono_ahorro.svg";
 import IconoCasa from "./../img/icono_casa.svg";
@@ -20,21 +28,35 @@ const Gasto = ({ gasto }) => {
     salud: IconoSalud,
     suscripciones: IconoSuscripciones,
   };
+  
+  const leadingActions = () => {
+    
+  }
+  const trailingActions = () => {
 
+  }
   return (
-    <div className="gasto sombra">
-      <div className="contenido-gasto">
-        <img src={diccionarioIconos[categoria]} alt={`Icono ${nombre}`} />
-        <div className="descripcion-gasto">
-          <p className="categoria">{categoria}</p>
-          <p className="nombre-gasto">{nombre}</p>
-          <p className="fecha-gasto">
-            Agregado el: {""} <span>{formatearFecha(fecha)}</span>
-          </p>
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions}
+        trailingActions={trailingActions}
+      >
+        <div className="gasto sombra">
+          <div className="contenido-gasto">
+            <img src={diccionarioIconos[categoria]} alt={`Icono ${nombre}`} />
+            <div className="descripcion-gasto">
+              <p className="categoria">{categoria}</p>
+              <p className="nombre-gasto">{nombre}</p>
+              <p className="fecha-gasto">
+                Agregado el: {""} <span>{formatearFecha(fecha)}</span>
+              </p>
+            </div>
+          </div>
+          <p className="cantidad-gasto">{formatearCantidad(cantidad)}</p>
         </div>
-      </div>
-      <p className="cantidad-gasto">{formatearCantidad(cantidad)}</p>
-    </div>
+
+      </SwipeableListItem>
+    </SwipeableList>
   );
 };
 
