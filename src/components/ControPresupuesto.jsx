63275aeac4be6db63c7032e27a4e2bda4ad8886e
Oscar Davila <oscar7822002@gmail.com>
2022-09-19 18:07:11 -0500

    Mostrando Textos en Gráfica Circular

--- src/components/ControPresupuesto.jsx
+++ src/components/ControPresupuesto.jsx
@@ -32,9 +32,11 @@ const ControPresupuesto = ({ gastos, presupuesto }) => {
       <div>
         <CircularProgressbar 
           value={porcentaje} 
+          text={`${porcentaje}% Gastado`}
           styles={buildStyles({
             pathColor: '#3B82F6',
             trailColor: '#F5F5F5',
+            textColor: '#3B82F6',
           })}
         />
       </div>
