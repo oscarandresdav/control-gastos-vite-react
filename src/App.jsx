579cd10a0034ef38df232f020e7937272cec59da
Oscar Davila <oscar7822002@gmail.com>
2022-09-19 18:18:08 -0500

    Colocar el Presupuesto en LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import { generarId } from "./helpers";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
@@ -19,6 +21,18 @@ function App() {
       mostrarModal();
     }
   }, [editarGasto]);
+
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0);
+  }, [presupuesto]);
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true);
+    }
+  }, []);
 
   const handleNuevoGasto = () => {
     mostrarModal();
